f293e56b5904590bfb0a11ffd1f3288c50aae2a3
dottokyo <envelopeaddiction@gmail.com>
2025-09-30 11:32:52 +0300

    commented limiter

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 from fastapi import FastAPI
+from fastapi import Request
 from pydantic import BaseModel
 import spacy
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
-from slowapi.errors import RateLimitExceeded
-import streamlit
+# from slowapi import Limiter, _rate_limit_exceeded_handler
+# from slowapi.util import get_remote_address
+# from slowapi.errors import RateLimitExceeded
 
 
 nlp = spacy.load("x5-ner")
 
-limiter = Limiter(key_func=get_remote_address)
+# limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
-app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+# app.state.limiter = limiter
+# app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 class PredictRequest(BaseModel):
     input: str
 
-class Render(BaseModel):
-    pass
-
-@app.get("/")
-def render(request: Render):
-    return streamlit hello
-
 @app.post("/api/predict")
-@limiter.limit("25/minute")
-async def predict(request: PredictRequest):
-    doc = nlp(request.input)
+# @limiter.limit("25/minute")
+async def predict(request: PredictRequest, req: Request):
+    print(f"Received request from {req.client.host}")
+    try:
+        doc = nlp(request.input)
+    except Exception as e:
+        return {"error": str(e)}
     entities = []
     for ent in doc.ents:
         entities.append({
